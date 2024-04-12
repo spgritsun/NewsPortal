64986c5b0ecd489ef78475655e892ceea97fc1f7
Sergei Gritsun <79272724+spgritsun@users.noreply.github.com>
2024-04-13 02:00:46 +0300

    Реализован почти весь функционал задания. Требует доработки фильтр цензурирования.

---
+++ main/templatetags/custom_filters.py
@@ -0,0 +1,18 @@
+from django import template
+
+register = template.Library()
+
+
+# Регистрируем наш фильтр под именем currency, чтоб Django понимал,
+# что это именно фильтр для шаблонов, а не простая функция.
+@register.filter()
+def censor(text):
+    if not isinstance(text, str):
+        raise ValueError("Фильтр цензурирования применяется только к строкам!")
+    censored_words = ["Скотина", "мразь", "Сволочь", "Идиот", "ублюдок", "редиска"]  # список слов для цензуры
+    for word in censored_words:
+        if word.upper() in text.upper():
+            text = text.replace(word, word[0] + "*"*len(word[1:]))
+
+    return text
+
