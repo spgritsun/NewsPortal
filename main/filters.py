79a9c2db45329e6a65a7ffc9771a78657d3f4002
Sergei Gritsun <79272724+spgritsun@users.noreply.github.com>
2024-04-18 17:12:11 +0300

    Пагинация и фильтрация реализованы.

--- main/filters.py
+++ main/filters.py
@@ -1,17 +1,25 @@
 from django_filters import FilterSet
+from django_filters.widgets import RangeWidget
+
 from .models import Post
+from django_filters import DateFromToRangeFilter
+
 
 # Создаем свой набор фильтров для модели Post.
 # FilterSet, который мы наследуем,
 # должен чем-то напомнить знакомые вам Django дженерики.
 class PostFilter(FilterSet):
-   class Meta:
-       # В Meta классе мы должны указать Django модель,
-       # в которой будем фильтровать записи.
-       model = Post
-       # В fields мы описываем по каким полям модели
-       # будет производиться фильтрация.
-       fields = {
-           # поиск по названию
-           'post_headline': ['icontains'],
-       }
+    post_time = DateFromToRangeFilter(field_name='post_time', label='Дата поста', widget=RangeWidget(attrs={'type':'date'}))
+
+    class Meta:
+        # В Meta классе мы должны указать Django модель,
+        # в которой будем фильтровать записи.
+        model = Post
+        # В fields мы описываем по каким полям модели
+        # будет производиться фильтрация.
+        fields = {
+            # поиск по названию
+            'post_headline': ['icontains'],
+            'post_text': ['icontains'],
+        }
+
--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 # Импортируем созданное нами представление
-from .views import PostList, NewsDetail, PostList1
+from .views import PostList, NewsDetail, PostList1, PostList2
 
 urlpatterns = [
    # Т.к. наше объявленное представление является классом,
    # а Django ожидает функцию, нам надо представить этот класс в виде view.
    # Для этого вызываем метод as_view.
    path('news/', PostList.as_view()),
-   path('posts/', PostList1.as_view()),
+   path('news/search/', PostList1.as_view()),
+   path('posts/', PostList2.as_view()),
+
    path('news/<int:pk>', NewsDetail.as_view()),
 ]
