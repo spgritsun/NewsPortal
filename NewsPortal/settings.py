1410bccc3b1feb337a2cc12e73eb38485edafb87
Sergei Gritsun <79272724+spgritsun@users.noreply.github.com>
2024-05-16 02:04:03 +0300

    Установлено: Redis, Celery. Проверена работоспособность.

--- NewsPortal/settings.py
+++ NewsPortal/settings.py
@@ -180,3 +180,11 @@ DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"
 APSCHEDULER_RUN_NOW_TIMEOUT = 25
+
+CELERY_BROKER_URL = 'redis://:KV85sYlBu86ZO1bPVZDlAAsCNUvMe0Op@redis-17910.c304.europe-west1-2.gce.redns.redis-cloud' \
+                    '.com:17910 '
+CELERY_RESULT_BACKEND = 'redis://:KV85sYlBu86ZO1bPVZDlAAsCNUvMe0Op@redis-17910.c304.europe-west1-2.gce.redns.redis' \
+                        '-cloud.com:17910 '
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
