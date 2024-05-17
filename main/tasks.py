e40d0c2a3de9b10748277fe3140807ae1ef1e7da
Sergei Gritsun <79272724+spgritsun@users.noreply.github.com>
2024-05-17 12:28:02 +0300

    Реализована рассылка статей за неделю. Периодическая задача задается через административную панель.

--- main/tasks.py
+++ main/tasks.py
@@ -1,9 +1,13 @@
+import datetime
+
 from celery import shared_task
 
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+from django.utils import timezone
 
 from NewsPortal.settings import SITE_URL, DEFAULT_FROM_EMAIL
+from main.models import Post, Category
 
 
 @shared_task
@@ -24,5 +28,33 @@ def send_notification(preview, pk, post_headline, subscribers_email_list, cat_na
         to=subscribers_email_list,
     )
 
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
+
+
+@shared_task
+def send_daily_posts():
+    today = timezone.now()
+    last_week = today - datetime.timedelta(days=7)
+    posts = Post.objects.filter(post_time__gte=last_week)  # Берем все посты за последнюю неделю
+    categories = set(posts.values_list('categories__category_name', flat=True))  # Получаем все названия категорий
+    # этих постов
+    subscribers = set(
+        Category.objects.filter(category_name__in=categories).values_list('subscribers__email', flat=True))  # Из
+    # объектов категорий постов извлекаем список email подписчиков на эти категории
+
+    html_content = render_to_string(
+        'daily_post.html',
+        {
+            'link': SITE_URL,
+            'posts': posts,
+        }
+    )
+    msg = EmailMultiAlternatives(
+        subject='Статьи за неделю',
+        body='',
+        from_email=DEFAULT_FROM_EMAIL,
+        to=subscribers,
+    )
     msg.attach_alternative(html_content, 'text/html')
     msg.send()
